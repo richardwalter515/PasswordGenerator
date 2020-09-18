f4d24301321a625f30b54028a3caa4666ae5bccc
Richard Walter <richardwalter515@gmail.com>
2020-09-18 14:41:43 -0400

    updated JS variables

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,19 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var howMany = prompt('How may characters would you like your password to be?').value;
+  var charSet = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+  function generatePassword() {
+    for (var i = 0; i <= howMany; i++) {
+      //add a character to the password
+    }
+  }
+  //generate password that is the number of characters the user enters
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 
 }
 
